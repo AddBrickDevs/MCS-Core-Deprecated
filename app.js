2710f7063eaebc0a2a718f8489db63e98bbd755b
ilouHD <marctroll99@gmail.com>
2015-09-30 16:41:56 +0200

    [+] Socket.IO-Verbindung

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var fs = require('fs');
 
 var log = require('./classes/log.js');
-var config = require('./classes/config.js');
+var Config = require('./classes/config.js');
 var version = require('./package.json').version;
 var Webserver = require('./classes/webserver.js');
 var Cloudserver = require('./classes/cloudlistener.js');
 var MySQL = require('MySQL');
-var mysqlclient = new MySQL(config.getMySQLHost(), config.getMySQLUser(), config.getMySQLPassword(), config.getMySQLDatabase(), config.getMySQLPoolSize());
+var io = require('socket.io');
+//var mysqlclient = new MySQL(Config.getMySQLHost(), Config.getMySQLUser(), Config.getMySQLPassword(), Config.getMySQLDatabase(), Config.getMySQLPoolSize());
 
 var Injector = require('./classes/injector/inject.js');
 var Hook = require('./classes/injector/hook.js');
