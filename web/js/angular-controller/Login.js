a59177a02e5560ba4545c84b2f103973e8c96c6f
unknown <marctroll99@gmail.com>
2016-04-01 02:27:46 +0200

    [*] finished Design, now you can choose whether to stay logged in or not

--- web/js/angular-controller/Login.js
+++ web/js/angular-controller/Login.js
@@ -3,19 +3,31 @@ app.controller('loginctrl', ['$scope', '$rootScope', 'Socket', '$location', '$co
     $scope.login = function() {
         var username = $scope.username;
         var password = $scope.password;
+        var stay = $scope.stay;
 
-        if(!$rootScope.loggedIn) {
-            Socket.emit("login-req", {username: username, password: password});
-            Socket.on("login-res", function(data) {
-                if(data.reason == "success") {
-                    $cookies.put("username", username);
-                    $cookies.put("session", data.session);
-                    $rootScope.loggedIn = true;
-                    $location.path("/");
-                } else {
-                    console.log(data.reason);
-                }
-            });
+        if(!username || !password) {
+            if(!username) {
+                $("#name").css("border-color", "#D40000");
+            }
+            if(!password) {
+                $("#password").css("border-color", "#D40000");
+            }
+        } else {
+            if(!$rootScope.loggedIn) {
+                Socket.emit("login-req", {username: username, password: password});
+                Socket.on("login-res", function(data) {
+                    if(data.reason == "success") {
+                        if(stay) {
+                            $cookies.put("username", username);
+                            $cookies.put("session", data.session);
+                        }
+                        $rootScope.loggedIn = true;
+                        $location.path("/");
+                    } else {
+                        console.log(data.reason);
+                    }
+                });
+            }
         }
     };
 
