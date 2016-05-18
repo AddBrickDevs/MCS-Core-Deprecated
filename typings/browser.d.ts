9de6f3e6730daed201d0cc3b46194d9f63d1fd89
Feeliiix <codeFeeliiix@gmail.com>
2016-05-18 14:31:51 +0200

    More .gitignore fails

--- typings/browser.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="browser/ambient/es6-shim/index.d.ts" />
-/// <reference path="browser/ambient/jasmine/index.d.ts" />
-/// <reference path="browser/ambient/node/index.d.ts" />
-/// <reference path="browser/ambient/socket.io-client/index.d.ts" />
